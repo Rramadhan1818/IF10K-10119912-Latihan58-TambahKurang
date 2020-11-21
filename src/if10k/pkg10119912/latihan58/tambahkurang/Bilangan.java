039cdadc53dc8bf1e245814c2de67815e60c95e7
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-21 19:07:03 +0700

    first add

---
+++ src/if10k/pkg10119912/latihan58/tambahkurang/Bilangan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class Bilangan {
+    int x;
+    int y;
+
+    public Bilangan(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/if10k/pkg10119912/latihan58/tambahkurang/IF10K10119912Latihan58TambahKurang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class IF10K10119912Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JumlahBilangan jb = new JumlahBilangan(3, 4);
+        jb.tampilHasilJumlah();
+        SelisihBilangan sb= new SelisihBilangan(3, 4);
+        sb.tampilSelisih();
+    }
+    
+}
